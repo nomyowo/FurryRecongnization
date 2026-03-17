88406efe0030ae40b58c076033cd90f2c53c892d
nomy <whitehattk@outlook.com>
2026-03-17 17:56:12 +0800

    docker优化

--- main.py
+++ main.py
@@ -549,4 +549,4 @@ def target_download(
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="127.0.0.1", port=5000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=5000, reload=True)
